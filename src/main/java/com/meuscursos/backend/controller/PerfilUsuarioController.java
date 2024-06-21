557e5ecce5a55ecfae8dba3e9a6fbf6f72d18c53
Wanderson Frauches <wfrauches89@gmail.com>
2024-06-20 21:22:35 -0300

    Alterações para conexão com frontend...

--- src/main/java/com/meuscursos/backend/controller/PerfilUsuarioController.java
+++ src/main/java/com/meuscursos/backend/controller/PerfilUsuarioController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/perfis-users")
+@RequestMapping("/perfil-usuario")
 public class PerfilUsuarioController {
 
     @Autowired
